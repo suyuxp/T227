713ccf252ec463c06832be4f4039aeef9071afb5
lin jun <117280858@qq.com>
2016-04-26 14:12:19 +0800

    添加跨域

--- src/common/bootstrap/middleware.js
+++ src/common/bootstrap/middleware.js
@@ -11,3 +11,15 @@
  * })
  * 
  */
+
+import cors from "think-cors";
+
+think.middleware("cors", cors);
+
+export default {
+    cors:{
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false 
+    }
+};
